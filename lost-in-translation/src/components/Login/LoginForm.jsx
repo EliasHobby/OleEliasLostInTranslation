58769acfa0f47ebd01c654209b05c1a5553646f3
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-09-16 12:51:15 +0200

    feat: added routing to paths

---
+++ lost-in-translation/src/components/Login/LoginForm.jsx
@@ -0,0 +1,14 @@
+import {useForm} from 'react-hook-form'
+
+const LoginForm = () => {
+    return (
+        <>
+            <h2>What's Your Name</h2>
+            <form>
+
+            </form>
+        </>
+    )
+}
+
+export default LoginForm
