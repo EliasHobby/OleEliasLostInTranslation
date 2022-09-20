6a466674cc12f6e4b4606743009423da43562acd
Elias Kløverød Brynestad <ELIAS-K-B@hotmail.com>
2022-09-20 14:39:27 +0200

    fix: restored file that broke during merge-conflict

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,29 @@
 import { NavLink } from "react-router-dom"
 import { useUser } from "../../context/UserContext"
+import {  } from "../../styles/navbar.css"
+
 
 //html for Navigation bar
 const Navbar = () => {
 
-    const {user} = useUser()
+    const { user } = useUser()
 
     return (
-        <nav>
-            { user !==null && //only show if a user is logged in
-            <ul> 
-                <li>
-                    <NavLink to= "translation">Translation</NavLink>
-                </li>
-                <li>
-                    <NavLink to="profile">Profile</NavLink>
-                </li>
-            </ul>
-            }
-
-
-        </nav>
+        <div className="Navigation">
+            <nav className="Navigation-container">
+                <h1 class="PageTitle">Lost In Translation</h1>
+                {user !== null && //only show if a user is logged in
+                    <ul>
+                        <li>
+                            <NavLink to="translation">Translation</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="profile">Profile</NavLink>
+                        </li>
+                    </ul>
+                }
+            </nav>
+        </div>
     )
 }
 
