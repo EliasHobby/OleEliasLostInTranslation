586abf53f77d950e328c6c6b6468ef0c13774a57
Elias Kløverød Brynestad <ELIAS-K-B@hotmail.com>
2022-09-20 11:55:13 +0200

    feat: added whitespace to website-body while maintaining 100% width on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,24 +4,24 @@ import { navigation } from "../../styles/navbar.css"
 
 const Navbar = () => {
 
-    const {user} = useUser()
+    const { user } = useUser()
 
     return (
-        <nav className="Navigation">
-            <h1 class="PageTitle">Lost In Translation</h1>
-            { user !==null &&
-            <ul> 
-                <li>
-                    <NavLink to= "translation">Translation</NavLink>
-                </li>
-                <li>
-                    <NavLink to="profile">Profile</NavLink>
-                </li>
-            </ul>
-            }
-
-
-        </nav>
+        <div className="Navigation">
+            <nav className="Navigation-container">
+                <h1 class="PageTitle">Lost In Translation</h1>
+                {user !== null &&
+                    <ul>
+                        <li>
+                            <NavLink to="translation">Translation</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="profile">Profile</NavLink>
+                        </li>
+                    </ul>
+                }
+            </nav>
+        </div>
     )
 }
 
