9b4849fdce2ea28fb9c20bed061c66c4b6dddc22
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-09-20 09:21:18 +0200

    refactor: allow for translations to be posted to user database

---
+++ lost-in-translation/src/api/translation.js
@@ -0,0 +1,25 @@
+import { createHeaders } from "."
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const addTranslation = async (user, translation) => {
+    try {
+        const response = await fetch(`${apiUrl}/${user.id}`, {
+            method: 'PATCH',
+            headers: createHeaders(),
+            body: JSON.stringify({
+                username: user.username,
+                translations: [...user.translations,translation]
+            })
+        })
+        if (!response.ok){
+            throw new Error('Could not add Order')
+        }
+        const result = await response.json()
+        return [ null, result]
+    }
+    catch(error){
+        return [ error.message, null]
+    }
+
+}
